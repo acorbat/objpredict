20a8000a6d4acd511bba0e6cbe1e1b4855e30b20
acorbat <acorbat@df.uba.ar>
2019-08-15 15:54:14 -0300

    Added documentation for main and proccess experiment files

--- __main__.py
+++ __main__.py
@@ -3,6 +3,8 @@ import argparse
 import process_experiment as exp
 
 def list_installed_pipelines(args):
+    """Prints a list of pipelines installed in the local directory and the
+    global directory."""
     for p in exp.get_installed_pipelines().keys():
         if p.startswith('./'):
             print(p + ' (local)')
@@ -11,10 +13,12 @@ def list_installed_pipelines(args):
 
 
 def _install(namespace):
+    """Installs the selected pipeline from local to global directory."""
     return exp.install(namespace.pipeline, namespace.name)
 
 
 def _process_experiment(namespace):
+    """Runs the selected classifier on the filepath"""
     return exp.process_experiment(namespace.filename, namespace.append, namespace.pipeline)
 
 
@@ -26,23 +30,28 @@ parser = argparse.ArgumentParser(prog='objpredict', description='Classify object
 
 subparsers = parser.add_subparsers()
 
+# Add the process experiment parse
 p_classif = subparsers.add_parser('predict', help='Classify objects from an objects CellProfiler *_Image.csv file.')
 p_classif.add_argument('filename', nargs='+')
 p_classif.add_argument('-p', '--pipeline')
 p_classif.add_argument('-a', '--append', default=False)
 p_classif.set_defaults(func=_process_experiment)
 
+# Parser for the listing of installed pipelines
 l_classif = subparsers.add_parser('list', help='List installed pipelines.')
 l_classif.set_defaults(func=list_installed_pipelines)
 
+# Reprints the list of pipelines installed
 u_classif = subparsers.add_parser('update', help='Update pipelines.')
 u_classif.set_defaults(func=list_installed_pipelines)
 
+# Parser for training the classifier
 t_classif = subparsers.add_parser('train', help='Train classifier.')
 t_classif.add_argument('folder', nargs='+')
 t_classif.add_argument('-o', '--output', default='MyPipeline')
 t_classif.set_defaults(func=_train)
 
+# Parser for installation of pipelines in global directory
 s_classif = subparsers.add_parser('install', help='Install pipelines.')
 s_classif.add_argument('pipeline')
 s_classif.add_argument('name', default=None)
